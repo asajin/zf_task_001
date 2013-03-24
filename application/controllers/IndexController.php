aac980d58dbf83b16ddfccef3dd62a75ef76e655
sajinandrei <sajinandrei@yahoo.fr>
2013-03-24 20:09:05 +0200

    #6 Result action - add comments in Index controller - integrate forward to result page if Import form validation is ok - integrate in result action check if form is valid - integrate render of email value from request in result template

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,11 +3,17 @@
 class IndexController extends Zend_Controller_Action
 {
 
+    /**
+     * Init of Index controller
+     */
     public function init()
     {
         /* Initialize action controller here */
     }
 
+    /**
+     * index action - show upload form and forward to result if validation ok
+     */
     public function indexAction()
     {
         // action body
@@ -15,7 +21,7 @@ class IndexController extends Zend_Controller_Action
         $form    = new Application_Form_Import();
         if ($this->getRequest()->isPost()) {
             if ($form->isValid($request->getPost())) {
-                // OK
+                $this->_forward('result');
             }
         }
 
@@ -23,9 +29,26 @@ class IndexController extends Zend_Controller_Action
         $this->view->form = $form;
     }
 
+    /**
+     * result action - validate upload form request data and show email
+     */
     public function resultAction()
     {
         // action body
+        $request = $this->getRequest();
+        $form    = new Application_Form_Import();
+
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($request->getPost())) {
+
+                $this->view->email  = $form->getValue('email');
+
+            } else {
+                $this->_forward('index');
+            }
+        } else {
+            $this->_forward('index');
+        }
     }
 
 
