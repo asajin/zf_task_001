58132929dcfb99ed96c5f60338c804049f1689e6
sajinandrei <sajinandrei@yahoo.fr>
2013-03-24 19:26:33 +0200

    #4 Import form - add comments - add input text form element for email - add input file element for upload file - add input submit form element

--- application/forms/Import.php
+++ application/forms/Import.php
@@ -1,11 +1,34 @@
 <?php
 
+/**
+ * Import form class
+ */
 class Application_Form_Import extends Zend_Form
 {
 
+    /**
+     * Init method of Import form class
+     */
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+        $this->setMethod('post');
+        $this->setAttrib('enctype', 'multipart/form-data');
+        
+        $this->addElement('text', 'email', array(
+            'label'      => 'Your email address:',
+            'required'   => true
+        ));
+        
+        $this->addElement('file', 'file', array(
+            'label'      => 'File to import:',
+            'destination'=> APPLICATION_PATH .'/../data/uploads',
+            'required'   => true
+        ));
+        
+        $this->addElement('submit', 'submit', array(
+            'ignore'   => true,
+            'label'    => 'Submit',
+        ));
     }
 
 
