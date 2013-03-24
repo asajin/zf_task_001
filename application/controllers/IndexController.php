f448e9d8559d1aff61dd956aa628654600c20a37
sajinandrei <sajinandrei@yahoo.fr>
2013-03-24 19:45:02 +0200

    #5 Index action - integrate instance of Import form in index action - integrate check if Import form is valid - integrate new page content for index action - integrate render of Import form

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,8 +11,17 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
-    }
+        $request = $this->getRequest();
+        $form    = new Application_Form_Import();
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                // OK
+            }
+        }
 
+        $form->setAction('index');
+        $this->view->form = $form;
+    }
 
 }
 
