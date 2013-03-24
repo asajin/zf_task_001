7f2bc2c74152b0a61d093f7b26ae4175b0ee2aa9
sajinandrei <sajinandrei@yahoo.fr>
2013-03-24 19:54:27 +0200

    #4 Import form - add filters and validation for email element - add validation for upload file element

--- application/forms/Import.php
+++ application/forms/Import.php
@@ -16,12 +16,21 @@ class Application_Form_Import extends Zend_Form
         
         $this->addElement('text', 'email', array(
             'label'      => 'Your email address:',
-            'required'   => true
+            'required'   => true,
+            'filters'    => array('StringTrim'),
+            'validators' => array(
+                'EmailAddress',
+            )
         ));
         
         $this->addElement('file', 'file', array(
             'label'      => 'File to import:',
-            'required'   => true
+            'required'   => true,
+            'validators' => array(
+                'Count' => array('min' => 1, 'max' => 1),
+                'Size' => array('min' => 1, 'max' => 1048576),
+                'Extension' => 'csv',
+            )
         ));
         
         $this->addElement('submit', 'submit', array(
